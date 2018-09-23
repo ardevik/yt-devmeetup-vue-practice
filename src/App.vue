ee06dfb5be8844b956f89e98657ba082fa4d8843
Muhammad Devikar <muhammad.devikar@gmail.com>
2018-09-23 12:59:16 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,7 @@
   <v-app>
     <v-navigation-drawer v-model="sideNav" app temporary>
       <v-list>
-        <v-list-tile 
-          v-for="item in menuItems" 
-          :key="item.title"
-          router
-          :to="item.link">
+        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-list-tile-action>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
@@ -17,20 +13,13 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app class="error">
-      <v-toolbar-side-icon 
-        @click.stop="sideNav = !sideNav"
-        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn 
-          v-for="item in menuItems" 
-          :key="item.title" 
-          flat
-          router
-          :to="item.link">
+        <v-btn v-for="item in menuItems" :key="item.title" flat router :to="item.link">
           <v-icon left>{{item.icon}}</v-icon>
             {{item.title}}
         </v-btn>
@@ -43,19 +32,19 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      sideNav: false,
-      menuItems: [
-        {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
-        {icon: 'room', title: 'Organize Meetup', link: '/meetup/new'},
-        {icon: 'person', title: 'Profile', link: '/profile'},
-        {icon: 'face', title: 'Sign up', link: '/signup'},
-        {icon: 'lock_open', title: 'Sign in', link: '/signin'}
-      ]
+  export default {
+    data () {
+      return {
+        sideNav: false,
+        menuItems: [
+          { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+          { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+          { icon: 'person', title: 'Profile', link: '/profile' },
+          { icon: 'face', title: 'Sign up', link: '/signup' },
+          { icon: 'lock_open', title: 'Sign in', link: '/signin' }
+        ]
+      }
     }
   }
-}
 </script>
  
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -29,26 +29,41 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      meetups: [
-        {imageUrl: 'http://3tsll33cscvk11pae33oze51-wpengine.netdna-ssl.com/wp-content/uploads/2015/04/jakarta-hidden-gems-travel.png', id: 'asdasgasasdfa', title: 'Meetup in Jakarta'},
-        {imageUrl: 'https://explorewisata.com/wp-content/uploads/2017/06/wisata-bandung.jpg', id: 'asdasgasasdfa', title: 'Meetup in Bandung'},
-        {imageUrl: 'https://cdn.idntimes.com/content-images/community/2018/05/surabaya-563208a10bae5-284a3da3c4964ff7cbd556970117db09_600x400.jpg', id: 'asdasgasasdfa', title: 'Meetup in Surabaya'}
-      ]
+  export default {
+    data () {
+      return {
+        meetups: [
+          {
+            imageUrl:
+              'http://3tsll33cscvk11pae33oze51-wpengine.netdna-ssl.com/wp-content/uploads/2015/04/jakarta-hidden-gems-travel.png',
+            id: 'asdasgasasdfa',
+            title: 'Meetup in Jakarta'
+          },
+          {
+            imageUrl:
+              'https://explorewisata.com/wp-content/uploads/2017/06/wisata-bandung.jpg',
+            id: 'asdasgasasdfa',
+            title: 'Meetup in Bandung'
+          },
+          {
+            imageUrl:
+              'https://cdn.idntimes.com/content-images/community/2018/05/surabaya-563208a10bae5-284a3da3c4964ff7cbd556970117db09_600x400.jpg',
+            id: 'asdasgasasdfa',
+            title: 'Meetup in Surabaya'
+          }
+        ]
+      }
     }
   }
-}
 </script>
 
 <style scoped>
   .titleself {
     position: absolute;
     bottom: 50px;
-    left:50%;
+    left: 50%;
     transform: translateX(-50%);
-    background-color: rgba(0, 0, 0, .5);
+    background-color: rgba(0, 0, 0, 0.5);
     color: white;
     font-size: 2em;
     padding: 20px;
